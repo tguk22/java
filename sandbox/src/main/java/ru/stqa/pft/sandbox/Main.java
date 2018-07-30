c08006ec8eca0977727fe18679cdd4dd54d7a7e4
tguk22 <41150498+tguk22@users.noreply.github.com>
2018-07-30 16:20:26 +0200

    Zmienione ustawienia formatowania

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Main.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Main.java
@@ -2,7 +2,7 @@ package ru.stqa.pft.sandbox;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, world");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello, world");
+  }
 }
